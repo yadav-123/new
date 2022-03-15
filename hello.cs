9f524ba39a93b49db0662d536e42b99a66f96600
yadav-123 <100979941+yadav-123@users.noreply.github.com>
2022-03-15 10:55:04 +0530

    Update hello.cs
    2

--- hello.cs
+++ hello.cs
@@ -6,7 +6,8 @@ namespace LeapYear
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = args[0];
+            Console.WriteLine("Hello {0}",name);
         }
     }
 }
